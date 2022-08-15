29b09a5923e5576192500161eb6365a892cb1e02
Leo Campos <leopcampos85@gmail.com>
2022-08-15 13:46:10 -0300

    CRUD Scaffold

---
+++ SalesWebMvcMySQL/Data/SalesWebMvcMySQLContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcMySQL.Models;
+
+namespace SalesWebMvcMySQL.Data
+{
+    public class SalesWebMvcMySQLContext : DbContext
+    {
+        public SalesWebMvcMySQLContext (DbContextOptions<SalesWebMvcMySQLContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcMySQL.Models.Department> Department { get; set; }
+    }
+}
